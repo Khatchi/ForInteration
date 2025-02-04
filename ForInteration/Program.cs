5838688481e784ccf57142f0176c1bf1cd2b5587
Khatchi <solomon9ice2014@gmail.com>
2025-02-04 20:44:56 +0100

    Arrays

--- ForInteration/Program.cs
+++ ForInteration/Program.cs
@@ -2,20 +2,35 @@
 {
     private static void Main(string[] args)
     {
-        for (int i = 0; i < 10;  i++)
-        {
-            //Console.WriteLine(i);
-            if (i == 7);
-            {
-                Console.WriteLine("Found seven!");
-                break;
-            }
-        }
+        //ForLoopInteration
 
-        for (int myValue = 0; myValue < 12; myValue++)
-        {
-            Console.WriteLine(myValue);
-        }
+        //for (int i = 0; i < 10;  i++)
+        //{
+        //    //Console.WriteLine(i);
+        //    if (i == 7);
+        //    {
+        //        Console.WriteLine("Found seven!");
+        //        break;
+        //    }
+        //}
+
+        //for (int myValue = 0; myValue < 12; myValue++)
+        //{
+        //    Console.WriteLine(myValue);
+        //}
+
+        /* Understanding Arrays*/
+
+        int[] numbers = new int[5];
+
+        numbers[0] = 4;
+        numbers[1] = 8;
+        numbers[2] = 15;
+        numbers[3] = 16;
+        numbers[4] = 23;
+
+        Console.WriteLine(numbers[1]);
+        Console.WriteLine(numbers.Length);
 
         Console.ReadLine();
     }
